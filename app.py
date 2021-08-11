f75cb4daed689087ec21d2b5b0039e9673fc4604
trapezius60 <34376406+trapezius60@users.noreply.github.com>
2021-08-11 13:19:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")#default page of web
-
-defindex():
-    returnrender_template('index.html')
+def index():
+    return render_template('index.html') 
 
 @app.route("/about")
-defabout():
-    returnrender_template('about.html')
+def about():
+    return render_template('about.html')
 
-if__name__=='__main__':
+if __name__ == '__main__':
     app.run()
